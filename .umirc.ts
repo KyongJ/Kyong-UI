eb2cf7ae72732384b7d85a8b6235fe0b7dd201c9
Kyong <kyongbest@163.com>
2023-03-10 02:18:40 +0800

    ci: update publicPath env

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,7 @@ const repo = 'Kyong-UI' // 项目名(也就是你的仓库名)
 
 export default defineConfig({
   title: 'KyongUI',
+  mode: 'site',
   favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
