f1aedaa8d7ed96b46527ae6fc5af0ca2c7aa5615
Kyong <kyongbest@163.com>
2023-03-10 02:10:08 +0800

    ci: update publicPath env

--- .umirc.ts
+++ .umirc.ts
@@ -4,15 +4,14 @@ const repo = 'Kyong-UI' // 项目名(也就是你的仓库名)
 
 export default defineConfig({
   title: 'KyongUI',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   locales: [
     ['zh-CN', '中文'],
     ['en-US', 'English'],
   ],
-  base: `/${repo}/`,
-  publicPath: `/${repo}/`,
+  base: process.env.SITE_BUILD_ENV === 'PREVIEW' ? undefined : `/${repo}`,
+  publicPath: process.env.SITE_BUILD_ENV === 'PREVIEW' ? undefined : `/${repo}/`,
   // more config: https://d.umijs.org/config
 })
